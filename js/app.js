0ac272abff2bb04df6470b1dd68b4cce4d5b1724
Michell Aagaard Dranig <tiamisse@gmail.com>
2020-12-01 15:28:10 +0100

    finished part 2 signup modal - missing responsiveness

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 // modals
 let loginModal = document.getElementById('loginModal');
 let signupModal = document.getElementById('signupModal');
+let signupModalTwo = document.getElementById('signupModalTwo');
 
 let loginBtn = document.getElementById('loginButton');
 let signupBtn = document.getElementById('signupButton');
 
 let spanLogin = document.getElementsByClassName('closeLoginModal')[0];
 let spanSignup = document.getElementsByClassName('closeSignupModal')[0];
+let spanSignupTwo = document.getElementsByClassName('closeSignupModalTwo')[0];
 
 loginBtn.onclick = function () {
   loginModal.style.display = 'block';
@@ -16,12 +18,22 @@ signupBtn.onclick = function () {
   signupModal.style.display = 'block';
 };
 
+function openSecondModal() {
+  loginModal.style.display = 'none';
+  signupModal.style.display = 'none';
+  signupModalTwo.style.display = 'block';
+}
+
 spanLogin.onclick = function () {
   loginModal.style.display = 'none';
 };
 
 spanSignup.onclick = function () {
   signupModal.style.display = 'none';
+};
+
+spanSignupTwo.onclick = function () {
+  signupModalTwo.style.display = 'none';
 };
 
 window.onclick = function (event) {
@@ -35,3 +47,20 @@ window.onclick = function (event) {
     signupModal.style.display = 'none';
   }
 };
+
+window.onclick = function (event) {
+  if (event.target == signupModalTwo) {
+    signupModalTwo.style.display = 'none';
+  }
+};
+
+function displayLoginModal() {
+  loginModal.style.display = 'block';
+  signupModal.style.display = 'none';
+  signupModalTwo.style.display = 'none';
+}
+
+function displaySignupModal() {
+  loginModal.style.display = 'none';
+  signupModal.style.display = 'block';
+}
