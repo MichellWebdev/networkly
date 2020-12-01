cf3fa72425440c80ac599e46def78a9fa255d5a1
Michell Aagaard Dranig <tiamisse@gmail.com>
2020-12-01 13:17:19 +0100

    initial work for signup modal

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
-// Login modal
+// modals
 let loginModal = document.getElementById('loginModal');
+let signupModal = document.getElementById('signupModal');
 
 let loginBtn = document.getElementById('loginButton');
+let signupBtn = document.getElementById('signupButton');
 
-let span = document.getElementsByClassName('closeLoginModal')[0];
+let spanLogin = document.getElementsByClassName('closeLoginModal')[0];
+let spanSignup = document.getElementsByClassName('closeSignupModal')[0];
 
 loginBtn.onclick = function () {
   loginModal.style.display = 'block';
 };
 
-span.onclick = function () {
+signupBtn.onclick = function () {
+  signupModal.style.display = 'block';
+};
+
+spanLogin.onclick = function () {
   loginModal.style.display = 'none';
+};
+
+spanSignup.onclick = function () {
+  signupModal.style.display = 'none';
 };
 
 window.onclick = function (event) {
   if (event.target == loginModal) {
     loginModal.style.display = 'none';
+  }
+};
+
+window.onclick = function (event) {
+  if (event.target == signupModal) {
+    signupModal.style.display = 'none';
   }
 };
